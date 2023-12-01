35fa8022a09171138e3baad10f1a3aa790e88930
koyuncu5362 <koyuncu.m@hotmail.com>
2023-12-01 16:29:21 +0300

    UseEffect (LifeCycle)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,18 @@
-import React,{useState} from 'react' 
+import React,{useState,useEffect} from 'react' 
 //for using state
 
 function Counter() {
     const [count,setCount]=useState(0);//default value 0 
     //setcount my func
     const [value,setValue]=useState(1);
+
+    useEffect(()=>{
+        console.log("Changed Any State");
+    });
+    useEffect(()=>{
+        console.log("Changed Count State");
+    },[count]);
+    
   return (
     <div>
         <h1>Count: {count}</h1>
