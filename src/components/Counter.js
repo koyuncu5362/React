dc6d1982eaeef780f513397045ad86b6eaf430fd
koyuncu5362 <koyuncu.m@hotmail.com>
2023-12-01 16:11:40 +0300

    Previous State

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,7 +8,7 @@ function Counter() {
   return (
     <div>
         <h1>Count: {count}</h1>
-        <button onClick={()=>setCount(count + value)}>
+        <button onClick={()=>setCount(prev=>prev+value)}>
             Count++
         </button>
         <hr></hr>
